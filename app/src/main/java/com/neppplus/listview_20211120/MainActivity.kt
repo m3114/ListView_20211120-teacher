23e559b4e6303e7ac316d295e0752fa1188ceb95
윤성노 <m3114@naver.com>
2021-11-20 13:45:28 +0900

    액티비디 목록공간 데이터 하나하나 타이핑해서 추가

--- app/src/main/java/com/neppplus/listview_20211120/MainActivity.kt
+++ app/src/main/java/com/neppplus/listview_20211120/MainActivity.kt
@@ -2,21 +2,19 @@ package com.neppplus.listview_20211120
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.listview_20211120.adapters.StudentAdapter
 import com.neppplus.listview_20211120.datas.StudentData
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    val mStudentList = arrayList<studentDate>()
-
+    lateinit var mStudentAdapter: StudentAdapter
+    val mStudentList = ArrayList<StudentData>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        mStutentList.add(StudentData("윤성노",1974, "서울시 중랑구"))
-
 
         mStudentList.add(StudentData("구영모",1990, "경기도 부천시"))
         mStudentList.add(StudentData("김정난",1976, "서울시 용산구"))
@@ -26,9 +24,6 @@ class MainActivity : AppCompatActivity() {
         mStudentList.add(StudentData("성연진",1994, "서울시 성동구"))
         mStudentList.add(StudentData("윤성노",1974, "서울시 중랑구"))
         mStudentList.add(StudentData("최연주",1985, "서울시 성북구"))
-
-
-
 
 
     }
